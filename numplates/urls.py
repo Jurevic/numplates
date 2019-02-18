4038e9a2a71c8434452d72334be44a15be43746c
RomualdasJurevicius <r.jurevicius@yahoo.com>
2019-02-18 08:16:16 +0200

    Adds media serving in debug mode
    Media setup and serving in debug mode

--- numplates/urls.py
+++ numplates/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework import routers
 
@@ -32,4 +33,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
